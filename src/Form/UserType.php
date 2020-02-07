c928e2ba3a539b08ab03e9178d9366d93e24f3d3
captainfrak <saez.sylvain@gmail.com>
2020-02-07 13:06:15 +0100

    add: add the *Type::class in the fields to improve the forms

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +15,17 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
-            ->add('password')
-            ->add('email')
+            ->add('username', TextType::class,[
+                'label' => 'Nom d\'utilisateur'
+            ])
+            ->add('password', PasswordType::class,[
+                'label' => 'Mot de passe',
+                'mapped' => false,
+                'required' => false,
+            ])
+            ->add('email', EmailType::class,[
+                'label' => 'Adresse Email'
+            ])
         ;
     }
 
